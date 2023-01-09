8067e356b39b3f1ee144423b3288adc60719d996
1hammad1 <hammadazam101010@gmail.com>
2023-01-08 20:21:30 -0800

    Login with Google and Facebook

---
+++ app/Http/Controllers/SocialController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialController extends Controller
+{
+    public function loginWithGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callbackFromGoogle()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+            // dd($user);
+
+            // Check Users Email If Already There
+            $is_user = User::where('email', $user->getEmail())->first();
+            if(!$is_user){
+
+                $saveUser = User::updateOrCreate([
+                    'google_id' => $user->getId(),
+                ],[
+                    'name' => $user->getName(),
+                    'email' => $user->getEmail(),
+                    'password' => Hash::make($user->getName().'@'.$user->getId())
+                ]);
+            }else{
+                $saveUser = User::where('email',  $user->getEmail())->update([
+                    'google_id' => $user->getId(),
+                ]);
+                $saveUser = User::where('email', $user->getEmail())->first();
+            }
+
+
+            Auth::loginUsingId($saveUser->id);
+
+            return redirect('/');
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function loginUsingFacebook()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function callbackFromFacebook()
+    {
+        try {
+            $user = Socialite::driver('facebook')->user();
+            dd($user);
+
+            $saveUser = User::updateOrCreate([
+                'facebook_id' => $user->getId(),
+            ],[
+                'name' => $user->getName(),
+                'email' => $user->getEmail(),
+                'password' => Hash::make($user->getName().'@'.$user->getId())
+            ]);
+
+            Auth::loginUsingId($saveUser->id);
+
+            return redirect()->route('home');
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+}
