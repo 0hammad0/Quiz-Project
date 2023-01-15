dcc6543d52e038630c69914f9b05aa81b69627db
1hammad1 <hammadazam101010@gmail.com>
2023-01-14 21:10:32 -0800

    update

--- config/services.php
+++ config/services.php
@@ -30,4 +30,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'client_id' => '685751402990-ce3qmuu6d9apmacrpup56f3pdisqr4ko.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect' => 'http://127.0.0.1:8000/google/callback/',
+    ],
+
+    'facebook' => [
+        'client_id' => '6214302101922046',
+        'client_secret' => 'c928d5cfa1d755bdc0f8a6b03ff81dad',
+        'redirect' => 'http://127.0.0.1:8000/facebook/callback/',
+    ],
+
 ];
